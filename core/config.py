f110b94c02580cffa10b024c5ce9c0ec45422dba
MOHAMMED REHAN <224443441+zfln-rehan0520@users.noreply.github.com>
2026-04-29 11:58:07 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,7 +1,21 @@
 import os
+import platform
 
-# This moves the vault to C:\Users\YourName\AppData\Local\ComradeVault
-VAULT_DIR = os.path.join(os.getenv('LOCALAPPDATA'), 'ComradeVault')
+# Detect the Operating System
+IS_WINDOWS = platform.system() == "Windows"
 
-# Rename the extension to look like a generic Windows data file
+if IS_WINDOWS:
+    # Windows: Use the Local AppData folder
+    # Fallback to current directory if LOCALAPPDATA isn't found for some reason
+    base_dir = os.getenv('LOCALAPPDATA', os.getcwd())
+    VAULT_DIR = os.path.join(base_dir, 'ComradeVault')
+else:
+    # Linux/Mac: Use a hidden folder in the User's Home directory
+    # ~/.comrade_vault is the standard Linux way to hide app data
+    base_dir = os.path.expanduser('~')
+    VAULT_DIR = os.path.join(base_dir, '.comrade_vault')
+
 VAULT_EXTENSION = ".dat"
+
+# Manifest path logic
+MANIFEST_PATH = os.path.join(VAULT_DIR, "sys_cache.idx")
