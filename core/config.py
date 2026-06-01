58187ed2f045c0bc5310f4dd263dc2798de3e2cc
MOHAMMED REHAN <codewithrehan0520@gmail.com>
2026-06-01 21:52:42 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,17 @@
+import os
+import platform
 
+IS_WINDOWS = platform.system() == "Windows"
+
+if IS_WINDOWS:
+   
+    base_dir = os.getenv('LOCALAPPDATA', os.getcwd())
+    VAULT_DIR = os.path.join(base_dir, 'ComradeVault')
+else:
+  
+    base_dir = os.path.expanduser('~')
+    VAULT_DIR = os.path.join(base_dir, '.comrade_vault')
+
+VAULT_EXTENSION = ".dat"
+
+MANIFEST_PATH = os.path.join(VAULT_DIR, "sys_cache.idx")
